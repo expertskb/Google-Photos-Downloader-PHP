0af8b926aa3aa946465d3e13e95148159e938cd0
Shakib Ahmed <112560247+expertskb@users.noreply.github.com>
2023-12-24 15:09:53 +0600

    UPDATED

---
+++ inc/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Author : Shakib Ahmed
+E-mail : shakibwap@yahoo.com
+WEB: https://shakib.eu.org
+*/
+
+$config = array(
+    "Title" => "Google Photos Downloader",
+    "Year" => date("Y"),
+    "Copyright" => 2023,
+    "_URL" => "https://shakib.eu.org",
+    "_AUTHOR" => "Shakib Ahmed"
+);
